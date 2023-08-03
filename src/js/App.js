9f8a9050e9c7de1cbed0585edcfb955e307aec9a
unknown <henriquescleverson@gmail.com>
2023-08-03 09:55:23 -0300

    React + Electron
    implementado o React funcionando com o Electron

---
+++ src/js/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../views/Home";
+import SideBar from "../components/SideBar";
+
+export default function App() {
+  return (
+    <Router>
+      <SideBar />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+
+        <Route path="/Tarefas">
+          <h1>Tarefas</h1>
+        </Route>
+
+        <Route path="/Contatos">
+          <h1>Contatos</h1>
+        </Route>
+
+        <Route path="/Lembrete">
+          <h1>Lembrete</h1>
+        </Route>
+
+        <Route path="/Senhas">
+          <h1>Senhas</h1>
+        </Route>
+
+        <Route path="/Configurações">
+          <h1>Configurações</h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
