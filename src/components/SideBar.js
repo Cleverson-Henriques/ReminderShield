36cf3cba56c2c2726d8f0296fa7318cecde11557
unknown <henriquescleverson@gmail.com>
2023-08-07 13:08:25 -0300

    Component Home
    Criado as rotas dos components

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,25 @@
 import React from "react";
-import "./SideBar.css";
 import { Link } from "react-router-dom";
+import { ImHome3, ImAddressBook, ImAlarm, ImKey, ImCog } from "react-icons/im";
 
 export default function SideBar() {
   return (
     <div className="sidebar">
-      <Link to="/Tarefas">TESTE</Link>
+      <Link to="/Tarefas">
+        <ImHome3 />
+      </Link>
+      <Link to="/Tarefas">
+        <ImAddressBook />
+      </Link>
+      <Link to="/Tarefas">
+        <ImKey />
+      </Link>
+      <Link to="/Tarefas">
+        <ImAlarm />
+      </Link>
+      <Link to="/Tarefas">
+        <ImCog />
+      </Link>
     </div>
   );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Home() {
-  return <h1>teste</h1>;
+  return (
+    <div className="main">
+      <h1>teste</h1>
+    </div>
+  );
 }
